81a630611e280e8be9e584863be7fed0c5d9cef1
Oliver Li <oliver.li@datadoghq.com>
2026-02-20 03:05:51 -0500

    export

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,13 @@
 import require$$0 from 'os';
 import require$$0$1 from 'crypto';
-import * as fs from 'fs';
-import fs__default from 'fs';
-import * as path from 'path';
-import path__default from 'path';
+import * as require$$1$1 from 'fs';
+import require$$1__default from 'fs';
+import * as require$$1 from 'path';
+import require$$1__default$1 from 'path';
 import require$$2 from 'http';
 import require$$3 from 'https';
 import require$$0$4 from 'net';
-import require$$1 from 'tls';
+import require$$1$2 from 'tls';
 import require$$4 from 'events';
 import require$$0$3 from 'assert';
 import require$$0$2 from 'util';
@@ -16,14 +16,14 @@ import require$$7 from 'buffer';
 import require$$8 from 'querystring';
 import require$$14 from 'stream/web';
 import require$$0$7 from 'node:stream';
-import require$$1$1 from 'node:util';
+import require$$1$3 from 'node:util';
 import require$$0$6 from 'node:events';
 import require$$0$8 from 'worker_threads';
 import require$$2$1 from 'perf_hooks';
 import require$$5 from 'util/types';
 import require$$4$1 from 'async_hooks';
-import require$$1$2 from 'console';
-import require$$1$3 from 'url';
+import require$$1$4 from 'console';
+import require$$1$5 from 'url';
 import require$$3$1 from 'zlib';
 import require$$6 from 'string_decoder';
 import require$$0$9 from 'diagnostics_channel';
@@ -224,7 +224,7 @@ function requireFileCommand () {
 	// We use any as a valid input type
 	/* eslint-disable @typescript-eslint/no-explicit-any */
 	const crypto = __importStar(require$$0$1);
-	const fs = __importStar(fs__default);
+	const fs = __importStar(require$$1__default);
 	const os = __importStar(require$$0);
 	const utils_1 = requireUtils$1();
 	function issueFileCommand(command, message) {
@@ -374,7 +374,7 @@ var hasRequiredTunnel$1;
 function requireTunnel$1 () {
 	if (hasRequiredTunnel$1) return tunnel$1;
 	hasRequiredTunnel$1 = 1;
-	var tls = require$$1;
+	var tls = require$$1$2;
 	var http = require$$2;
 	var https = require$$3;
 	var events = require$$4;
@@ -1756,7 +1756,7 @@ function requireSbmh () {
 	 * by Hongli Lai at: https://github.com/FooBarWidget/boyer-moore-horspool
 	 */
 	const EventEmitter = require$$0$6.EventEmitter;
-	const inherits = require$$1$1.inherits;
+	const inherits = require$$1$3.inherits;
 
 	function SBMH (needle) {
 	  if (typeof needle === 'string') {
@@ -1965,7 +1965,7 @@ function requirePartStream () {
 	if (hasRequiredPartStream) return PartStream_1;
 	hasRequiredPartStream = 1;
 
-	const inherits = require$$1$1.inherits;
+	const inherits = require$$1$3.inherits;
 	const ReadableStream = require$$0$7.Readable;
 
 	function PartStream (opts) {
@@ -2011,7 +2011,7 @@ function requireHeaderParser () {
 	hasRequiredHeaderParser = 1;
 
 	const EventEmitter = require$$0$6.EventEmitter;
-	const inherits = require$$1$1.inherits;
+	const inherits = require$$1$3.inherits;
 	const getLimit = requireGetLimit();
 
 	const StreamSearch = requireSbmh();
@@ -2119,7 +2119,7 @@ function requireDicer () {
 	hasRequiredDicer = 1;
 
 	const WritableStream = require$$0$7.Writable;
-	const inherits = require$$1$1.inherits;
+	const inherits = require$$1$3.inherits;
 
 	const StreamSearch = requireSbmh();
 
@@ -2696,7 +2696,7 @@ function requireMultipart () {
 	//     -- this will require modifications to utils.parseParams
 
 	const { Readable } = require$$0$7;
-	const { inherits } = require$$1$1;
+	const { inherits } = require$$1$3;
 
 	const Dicer = requireDicer();
 
@@ -3262,7 +3262,7 @@ function requireMain () {
 	hasRequiredMain = 1;
 
 	const WritableStream = require$$0$7.Writable;
-	const { inherits } = require$$1$1;
+	const { inherits } = require$$1$3;
 	const Dicer = requireDicer();
 
 	const MultipartParser = requireMultipart();
@@ -8086,7 +8086,7 @@ function requireConnect () {
 	    let socket;
 	    if (protocol === 'https:') {
 	      if (!tls) {
-	        tls = require$$1;
+	        tls = require$$1$2;
 	      }
 	      servername = servername || options.servername || util.getServerName(host) || null;
 
@@ -14110,7 +14110,7 @@ function requirePendingInterceptorsFormatter () {
 	hasRequiredPendingInterceptorsFormatter = 1;
 
 	const { Transform } = require$$0$5;
-	const { Console } = require$$1$2;
+	const { Console } = require$$1$4;
 
 	/**
 	 * Gets the output of `console.table(…)` as a string.
@@ -14337,7 +14337,7 @@ function requireProxyAgent () {
 	hasRequiredProxyAgent = 1;
 
 	const { kProxy, kClose, kDestroy, kInterceptors } = requireSymbols$4();
-	const { URL } = require$$1$3;
+	const { URL } = require$$1$5;
 	const Agent = requireAgent();
 	const Pool = requirePool();
 	const DispatcherBase = requireDispatcherBase();
@@ -25202,7 +25202,7 @@ function requireSummary () {
 		Object.defineProperty(exports$1, "__esModule", { value: true });
 		exports$1.summary = exports$1.markdownSummary = exports$1.SUMMARY_DOCS_URL = exports$1.SUMMARY_ENV_VAR = void 0;
 		const os_1 = require$$0;
-		const fs_1 = fs__default;
+		const fs_1 = require$$1__default;
 		const { access, appendFile, writeFile } = fs_1.promises;
 		exports$1.SUMMARY_ENV_VAR = 'GITHUB_STEP_SUMMARY';
 		exports$1.SUMMARY_DOCS_URL = 'https://docs.github.com/actions/using-workflows/workflow-commands-for-github-actions#adding-a-job-summary';
@@ -25508,7 +25508,7 @@ function requirePathUtils () {
 	};
 	Object.defineProperty(pathUtils, "__esModule", { value: true });
 	pathUtils.toPlatformPath = pathUtils.toWin32Path = pathUtils.toPosixPath = void 0;
-	const path = __importStar(path__default);
+	const path = __importStar(require$$1__default$1);
 	/**
 	 * toPosixPath converts the given path to the posix form. On Windows, \\ will be
 	 * replaced with /.
@@ -25594,8 +25594,8 @@ function requireIoUtil () {
 		var _a;
 		Object.defineProperty(exports$1, "__esModule", { value: true });
 		exports$1.getCmdPath = exports$1.tryGetExecutablePath = exports$1.isRooted = exports$1.isDirectory = exports$1.exists = exports$1.READONLY = exports$1.UV_FS_O_EXLOCK = exports$1.IS_WINDOWS = exports$1.unlink = exports$1.symlink = exports$1.stat = exports$1.rmdir = exports$1.rm = exports$1.rename = exports$1.readlink = exports$1.readdir = exports$1.open = exports$1.mkdir = exports$1.lstat = exports$1.copyFile = exports$1.chmod = void 0;
-		const fs = __importStar(fs__default);
-		const path = __importStar(path__default);
+		const fs = __importStar(require$$1__default);
+		const path = __importStar(require$$1__default$1);
 		_a = fs.promises
 		// export const {open} = 'fs'
 		, exports$1.chmod = _a.chmod, exports$1.copyFile = _a.copyFile, exports$1.lstat = _a.lstat, exports$1.mkdir = _a.mkdir, exports$1.open = _a.open, exports$1.readdir = _a.readdir, exports$1.readlink = _a.readlink, exports$1.rename = _a.rename, exports$1.rm = _a.rm, exports$1.rmdir = _a.rmdir, exports$1.stat = _a.stat, exports$1.symlink = _a.symlink, exports$1.unlink = _a.unlink;
@@ -25785,7 +25785,7 @@ function requireIo () {
 	Object.defineProperty(io, "__esModule", { value: true });
 	io.findInPath = io.which = io.mkdirP = io.rmRF = io.mv = io.cp = void 0;
 	const assert_1 = require$$0$3;
-	const path = __importStar(path__default);
+	const path = __importStar(require$$1__default$1);
 	const ioUtil = __importStar(requireIoUtil());
 	/**
 	 * Copies a file or folder.
@@ -26093,7 +26093,7 @@ function requireToolrunner () {
 	const os = __importStar(require$$0);
 	const events = __importStar(require$$4);
 	const child = __importStar(require$$2$2);
-	const path = __importStar(path__default);
+	const path = __importStar(require$$1__default$1);
 	const io = __importStar(requireIo());
 	const ioUtil = __importStar(requireIoUtil());
 	const timers_1 = require$$6$1;
@@ -26937,7 +26937,7 @@ function requireCore () {
 		const file_command_1 = requireFileCommand();
 		const utils_1 = requireUtils$1();
 		const os = __importStar(require$$0);
-		const path = __importStar(path__default);
+		const path = __importStar(require$$1__default$1);
 		const oidc_utils_1 = requireOidcUtils();
 		/**
 		 * The code to exit an action
@@ -27264,17 +27264,11 @@ async function run() {
         const datadogAppKey = coreExports.getInput('datadog-app-key', {
             required: true
         });
-        const appName = coreExports.getInput('app-name', { required: true });
-        const appDisplayName = coreExports.getInput('app-display-name', {
-            required: true
-        });
-        const appDirectory = path.resolve(coreExports.getInput('app-directory') || '.');
-        const buildDir = coreExports.getInput('build-dir') || 'dist';
+        const appDirectory = require$$1.resolve(coreExports.getInput('app-directory') || '.');
         const installCommand = coreExports.getInput('install-command') || 'npm ci';
         const buildCommand = coreExports.getInput('build-command') || 'npm run build';
-        const datadogSite = coreExports.getInput('datadog-site') || 'app.datadoghq.com';
         // Verify app directory exists
-        if (!fs.existsSync(appDirectory)) {
+        if (!require$$1$1.existsSync(appDirectory)) {
             throw new Error(`App directory '${appDirectory}' does not exist`);
         }
         coreExports.info(`App directory found: ${appDirectory}`);
@@ -27287,50 +27281,25 @@ async function run() {
             await execExports.exec(installCmd, installCmdArgs, { cwd: appDirectory });
             coreExports.info('✓ Dependencies installed successfully');
         }
-        // Step 2: Build the Vite app
+        // Step 2: Build the Vite app (with upload)
+        const gitSha = process.env.GITHUB_SHA || '';
         coreExports.info(`Building Vite app with command: ${buildCommand}`);
+        coreExports.info(`Git commit SHA: ${gitSha}`);
         const buildArgs = buildCommand.split(' ');
         const buildCmd = buildArgs[0];
         const buildCmdArgs = buildArgs.slice(1);
-        await execExports.exec(buildCmd, buildCmdArgs, { cwd: appDirectory });
-        coreExports.info('✓ Build completed successfully');
-        // Step 3: Verify build directory exists
-        const fullBuildPath = path.join(appDirectory, buildDir);
-        if (!fs.existsSync(fullBuildPath)) {
-            throw new Error(`Build directory '${fullBuildPath}' does not exist`);
-        }
-        coreExports.info(`✓ Build directory '${fullBuildPath}' exists`);
-        // Step 4: Create a zip file of the build directory contents
-        const zipFile = 'dist.zip';
-        coreExports.info(`Creating zip file: ${zipFile}`);
-        // Zip contents without the directory wrapper
-        await execExports.exec('zip', ['-r', `../${zipFile}`, '.'], { cwd: fullBuildPath });
-        coreExports.info(`✓ Zip file created: ${zipFile}`);
-        // Step 5: Upload to Datadog
-        const uploadUrl = `https://${datadogSite}/api/unstable/app-builder-code/apps/${appName}/upload`;
-        coreExports.info(`Uploading to Datadog: ${uploadUrl}`);
-        await execExports.exec('curl', [
-            '-X',
-            'POST',
-            uploadUrl,
-            '-H',
-            'Accept: application/json',
-            '-H',
-            `DD-API-KEY: ${datadogApiKey}`,
-            '-H',
-            `DD-APPLICATION-KEY: ${datadogAppKey}`,
-            '-F',
-            `bundle=@${zipFile}`,
-            '-F',
-            `name=${appDisplayName}`
-        ], { cwd: appDirectory });
-        coreExports.info(''); // Empty line for formatting
-        coreExports.info('✓ Upload completed successfully');
-        coreExports.info(`App '${appDisplayName}' has been deployed to Datadog! 🎉`);
-        // Clean up zip file
-        const zipFilePath = path.join(appDirectory, zipFile);
-        fs.unlinkSync(zipFilePath);
-        coreExports.debug('Cleaned up temporary zip file');
+        await execExports.exec(buildCmd, buildCmdArgs, {
+            cwd: appDirectory,
+            env: {
+                ...process.env,
+                DATADOG_API_KEY: datadogApiKey,
+                DATADOG_APP_KEY: datadogAppKey,
+                DATADOG_APPS_VERSION_NAME: gitSha,
+                APPS_UPLOAD_ASSETS: '1'
+            }
+        });
+        coreExports.info('✓ Build and upload completed successfully');
+        coreExports.info(`Your app has been deployed to Datadog! 🎉`);
     }
     catch (error) {
         // Fail the workflow run if an error occurs
